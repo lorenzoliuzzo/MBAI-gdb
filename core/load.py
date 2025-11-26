53970bbf1f2710c7c8dd5bfc0e84cfb5097d0b62
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2025-11-26 20:46:14 +0100

    small refactoring in load.py

--- core/load.py
+++ core/load.py
@@ -6,8 +6,6 @@ from core.driver import get_driver
 
 from graph import get_teams
 
-# from core.router import get_season_path
-
 
 def load_periods(session, game_id, periods): 
     print(f"Creating `Period`s for `Game` {game_id}...")
@@ -15,19 +13,17 @@ def load_periods(session, game_id, periods):
     if len(periods) < 4: 
         return 
 
-    MERGE_PERIOD_TX = lambda tx: tx.run(MERGE_PERIOD, 
-        game_id=game_id, periods=periods
-    )
+    MERGE_PERIOD_TX = lambda tx: tx.run(MERGE_PERIOD, game_id=game_id, periods=periods)
     session.execute_write(MERGE_PERIOD_TX)
 
-    MERGE_NEXT_PERIOD_LINK_TX = lambda tx: tx.run(MERGE_NEXT_PERIOD_LINK, 
-        game_id=game_id
-    )
+    MERGE_NEXT_PERIOD_LINK_TX = lambda tx: tx.run(MERGE_NEXT_PERIOD_LINK, game_id=game_id)
     session.execute_write(MERGE_NEXT_PERIOD_LINK_TX)
 
+    SET_GAME_DURATION_TX = lambda tx: tx.run(SET_GAME_DURATION, game_id=game_id)
+    session.execute_write(SET_GAME_DURATION_TX)
 
 
-def load_lineups(session, game_id: int, starters: pd.DataFrame, subs: pd.DataFrame): 
+def load_lineups(session, game_id: int, subs: pd.DataFrame, starters: pd.DataFrame): 
     print(f"Creating `LineUp`'s for `Game` {game_id}...")
     
     ht_id, at_id = get_teams(session, game_id)
@@ -36,14 +32,13 @@ def load_lineups(session, game_id: int, starters: pd.DataFrame, subs: pd.DataFra
         team_subs = subs[subs['teamId'] == team_id]
         
         lineups = extract_lineups(starter_ids, team_subs)
-        MERGE_LINEUPS_TX = lambda tx:tx.run(MERGE_LINEUPS, 
-            game_id=game_id, team_id=team_id, lineups=lineups
+        MERGE_LINEUPS_TX = lambda tx:tx.run(MERGE_LINEUPS, game_id=game_id, 
+            team_id=team_id, lineups=lineups
         )
         session.execute_write(MERGE_LINEUPS_TX)
 
     MERGE_NEXT_LINEUP_LINK_TX = lambda tx: tx.run(MERGE_NEXT_LINEUP_LINK, game_id=game_id)
     session.execute_write(MERGE_NEXT_LINEUP_LINK_TX)
-
 
 
 def load_game(season_id, game_id):
@@ -53,34 +48,26 @@ def load_game(season_id, game_id):
 
     print(f"Creating a new game: {game_id}")
 
-    # filename = get_season_path(season_id) / "games" / f"g{game_id}.csv"
     pbp_df = None
     try: 
-        # pbp_df = pd.read_csv(filename)
         pbp_df = fetch_pbp(game_id)
     except Exception as e: 
         print(f"Some error occured while reading the game actions from {filename}: {e}")
-    
+        return 
+
     boxscore_df = None
     try: 
         boxscore_df = fetch_boxscore(game_id)
     except Exception as e: 
         print(f"Some error occured while fetching the game boxscore: {e}")
-
-    # if not pbp_df or not boxscore_df: 
-    #     return
+        return
     
+    periods = extract_periods(pbp_df)
+    subs = extract_subs(pbp_df)
+    starters = extract_starters(boxscore_df)
+
     with driver.session() as session:
-        periods = extract_periods(pbp_df)
-        starters = extract_starters(boxscore_df)
-        subs = extract_subs(pbp_df)
-
         load_periods(session, game_id, periods)
-
-        SET_GAME_DURATION_TX = lambda tx: tx.run(SET_GAME_DURATION, game_id=game_id)
-        session.execute_write(SET_GAME_DURATION_TX)
-
-        load_lineups(session, game_id, starters, subs)
+        load_lineups(session, game_id, subs, starters)
 
     driver.close()
-
